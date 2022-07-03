af4bf625ed9cc8d72fc5abff4c2a417aef164bf2
jeffrey <piccirilloj1@gmail.com>
2022-07-03 12:03:32 -0500

    Implement french canadian translation set

---
+++ src/assets/locales/fr_ca.js
@@ -0,0 +1,84 @@
+import keys from './keys.js';
+import { fr } from 'vuetify/lib/locale/';
+
+export default {
+  ...fr, // existing vuetify translation entries
+  [keys.FOOTER_COPYWRIGHT]: 'French canadian version of copywright footer',
+  [keys.INTERPRETATION_MILD]: 'Licht',
+  [keys.INTERPRETATION_MODERATE]: 'Matig',
+  [keys.INTERPRETATION_SEVERE]: 'Ernstig',
+  [keys.HEADER_TITLE]:
+    'Score du résultat nasal pour l’épistaxis liée à la télangiectasie hémorragique héréditaire (NOSE-HHT)',
+  [keys.HEADER_BODY_1]:
+    'Vous trouverez ci-dessous une liste des incidences physiques, fonctionnelles et émotionnelles de vos saignements de nez. Nous aimerions en connaître davantage au sujet de ces problèmes et vous serions reconnaissants de répondre aux questions suivantes du mieux que vous le pouvez. Il n’y a pas de bonnes ou de mauvaises réponses, car vos réponses vous sont propres. Veuillez évaluer les problèmes tels que vous les avez éprouvés au cours des ',
+  [keys.HEADER_BODY_2]: 'deux dernières semaines.',
+  [keys.HEADER_BODY_3]: '',
+  [keys.HEADER_BODY_4]: 'Nous vous remercions de votre participation.',
+
+  [keys.SECTION1_INSTRUCTIONS_1]: 'Veuillez évaluer les problèmes suivants ',
+  [keys.SECTION1_INSTRUCTIONS_2]: 'dus à vos saignements de nez',
+  [keys.SECTION1_INSTRUCTIONS_3]:
+    ' en encerclant le chiffre qui correspond à la gravité de chaque problème :',
+
+  [keys.SECTION2_INSTRUCTIONS_1]:
+    'Veuillez évaluer à quel point il est difficile pour vous d’effectuer les tâches suivantes ',
+  [keys.SECTION2_INSTRUCTIONS_2]: 'en raison de vos saignements de nez ',
+  [keys.SECTION2_INSTRUCTIONS_3]: ':',
+
+  [keys.SECTION3_INSTRUCTIONS_1]:
+    'Veuillez évaluer à quelle fréquence vous êtes incommodé(e) par chacun des énoncés suivants ',
+  [keys.SECTION3_INSTRUCTIONS_2]: 'en raison de vos saignements de nez ',
+  [keys.SECTION3_INSTRUCTIONS_3]: ':',
+
+  [keys.SECTION1_OPTION_1]: 'Aucun problème',
+  [keys.SECTION1_OPTION_2]: 'Problème léger',
+  [keys.SECTION1_OPTION_3]: 'Problème modéré',
+  [keys.SECTION1_OPTION_4]: 'Problème grave',
+  [keys.SECTION1_OPTION_5]: 'Problème aussi grave que possible',
+
+  [keys.SECTION2_OPTION_1]: 'Aucune difficulté',
+  [keys.SECTION2_OPTION_2]: 'Légère difficulté',
+  [keys.SECTION2_OPTION_3]: 'Difficulté modérée',
+  [keys.SECTION2_OPTION_4]: 'Grave difficulté',
+  [keys.SECTION2_OPTION_5]: 'Difficulté extrême',
+
+  [keys.SECTION3_OPTION_1]: 'Pas incommodé(e)',
+  [keys.SECTION3_OPTION_2]: 'Très rarement incommodé(e)',
+  [keys.SECTION3_OPTION_3]: 'Rarement incommodé(e)',
+  [keys.SECTION3_OPTION_4]: 'Souvent incommodé(e)',
+  [keys.SECTION3_OPTION_5]: 'Très souvent incommodé(e)',
+
+  [keys.SECTION1_PROMPT_1]: 'Sang qui coule à l’arrière de votre gorge',
+  [keys.SECTION1_PROMPT_2]: 'Nez bouché/congestionné',
+  [keys.SECTION1_PROMPT_3]: 'Croûtes dans le nez',
+  [keys.SECTION1_PROMPT_4]: 'Fatigue',
+  [keys.SECTION1_PROMPT_5]: 'Essoufflement',
+  [keys.SECTION1_PROMPT_6]: 'Diminution de l’odorat/du goût',
+
+  [keys.SECTION2_PROMPT_1]: 'Se moucher',
+  [keys.SECTION2_PROMPT_2]: 'Se pencher/ramasser quelque chose au sol',
+  [keys.SECTION2_PROMPT_3]: 'Respirer par le nez',
+  [keys.SECTION2_PROMPT_4]: 'Faire de l’exercice',
+  [keys.SECTION2_PROMPT_5]: 'Accomplir vos tâches au travail (ou à l’école)',
+  [keys.SECTION2_PROMPT_6]: 'Rester endormi(e)',
+  [keys.SECTION2_PROMPT_7]: 'Profiter du temps avec vos amis/votre famille',
+  [keys.SECTION2_PROMPT_8]: 'Manger certains aliments (p. ex. aliments épicés)',
+  [keys.SECTION2_PROMPT_9]:
+    'Avoir des relations intimes avec votre conjoint(e) ou votre partenaire ',
+  [keys.SECTION2_PROMPT_10]: 'Voyager (p. ex. en avion) ',
+  [keys.SECTION2_PROMPT_11]: 'S’endormir ',
+  [keys.SECTION2_PROMPT_12]: 'Nettoyer votre maison/appartement',
+  [keys.SECTION2_PROMPT_13]: 'Aller dehors, quel que soit le temps ou la saison',
+  [keys.SECTION2_PROMPT_14]: 'Cuisiner/préparer des repas',
+
+  [keys.SECTION3_PROMPT_1]: 'Peur de saigner du nez en public',
+  [keys.SECTION3_PROMPT_2]:
+    'Peur de ne pas savoir quand se produira votre prochain saignement de nez',
+  [keys.SECTION3_PROMPT_3]: 'Avoir du sang sur vos vêtements',
+  [keys.SECTION3_PROMPT_4]: 'Peur de ne pas pouvoir arrêter un saignement de nez',
+  [keys.SECTION3_PROMPT_5]: 'Gêne',
+  [keys.SECTION3_PROMPT_6]: 'Frustration/agitation/irritabilité',
+  [keys.SECTION3_PROMPT_7]: 'Baisse de concentration ',
+  [keys.SECTION3_PROMPT_8]: 'Tristesse',
+  [keys.SECTION3_PROMPT_9]: 'Le besoin d’acheter de nouveaux vêtements',
+};
--- src/assets/locales/index.js
+++ src/assets/locales/index.js
@@ -3,6 +3,7 @@ import es_us from './es_us';
 import es_sp from './es_sp';
 import nl_nl from './nl_nl';
 import it_it from './it_it';
+import fr_ca from './fr_ca';
 
 export default {
   en_us,
@@ -10,4 +11,5 @@ export default {
   es_sp,
   nl_nl,
   it_it,
+  fr_ca,
 };
