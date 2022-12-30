da0c7271f212945b9f2952fcf73f0a4487849f2d
jeffrey <piccirilloj1@gmail.com>
2022-12-30 15:08:01 -0600

    Add manual preview / embedded meta tags for ODOR questionnaire

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const webpackParams = {
+  title: 'Olfactory Dysfunction Outcomes Rating (ODOR) Questionnaire',
+  description:
+    "Evaluate the extent of one's physical problems, limitations, and emotional consequences of olfactory dysfunfunction with a 28-question inventory.",
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/odor/',
+};
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/odor/' : '/',
   transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value;
+      });
+      return args;
+    });
+  },
 };
